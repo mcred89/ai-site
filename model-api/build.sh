b03cf7b7be913f5322c3161c39a0489617589cdc
mac <seanmcilroy89@gmail.com>
2018-12-15 16:45:56 -0600

    getting serverless setup

--- model-api/build.sh
+++
@@ -1,2 +0,0 @@
-export WORK_DIR=$(pwd)
-zip -r ${WORK_DIR}/ai_api_build.zip predictors index.py
--- model-api/layers/flask.sh
+++
@@ -1,4 +0,0 @@
-export WORK_DIR=$(pwd)
-pip install flask
-cd venv/Lib/site-packages
-zip -r ${WORK_DIR}/flask-layer.zip .
