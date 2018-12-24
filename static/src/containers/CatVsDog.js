d74dfccccb98411d4b745935cd7a744beaf2bb21
mac <seanmcilroy89@gmail.com>
2018-12-24 08:54:25 -0600

    fixed method names and indentation

--- static/src/containers/CatVsDog.js
+++ static/src/containers/CatVsDog.js
@@ -16,30 +16,23 @@ export class CatVsDog extends Component {
         };
         this.handleSubmit = this.handleSubmit.bind(this);
         this.fileInput = React.createRef();
-        this.getImage = this.getImage.bind(this);
         this.loadModels = this.loadModels.bind(this);
         this.predictImage = this.predictImage.bind(this);
         this.handleFile = this.handleFile.bind(this);
-      }
+    }
 
-      handleSubmit(event) {
+    handleSubmit(event) {
         event.preventDefault();
-        this.getImage(this.state.src);
+        this.predictImage(this.state.src);
+    }
 
-      }
-
-      predictImage(image) {
-        let features = this.state.vgg16.predict(image);
-        let predict = this.state.model.predict(features);
-        return predict
-      }
-
-      getImage(img) {
+    predictImage(img) {
         let response
         const tensor = tf.fromPixels(img);
         let meanImageNetRGB= tf.tensor1d([123.68,116.779,103.939]);
         const batched = tensor.sub(meanImageNetRGB).reverse(2).expandDims(0);
-        const prediction = this.predictImage(batched).as1D().argMax().dataSync()[0]
+        let features = this.state.vgg16.predict(batched);
+        let prediction = this.state.model.predict(features).as1D().argMax().dataSync()[0];
         if (prediction === 0 ) {
             response = 'Cat'
         } else if (prediction === 1 ) {
@@ -49,12 +42,10 @@ export class CatVsDog extends Component {
             prediction: response,
             displayImage: URL.createObjectURL(this.fileInput.current.files[0])
         });
-        console.log(prediction)
-        return batched
-      }
+        console.log('Prediction: ' + prediction)
+    }
 
-      handleFile(event) {
-        console.log('handleFile')
+    handleFile(event) {
         let file = event.target.files[0]
         let reader = new FileReader()
         let self = this
@@ -69,22 +60,20 @@ export class CatVsDog extends Component {
         }
         let read_file = reader.readAsDataURL(file);
         self.setState({ imageValue: read_file });
+    }
 
-      }
-
-
-      componentDidMount() {
+    componentDidMount() {
         this.loadModels()
-      }
+    }
 
-      loadModels = async() => {
+    loadModels = async() => {
         this.setState({
             vgg16: await tf.loadModel('https://s3-us-west-2.amazonaws.com/testing-models/headless_vgg16/model.json'),
             model: await tf.loadModel('https://s3-us-west-2.amazonaws.com/testing-models/catvsdog_classifier2/model.json')
         })
-        }
+    }
     
-      render() {
+    render() {
         return (
             <div className="row">
                 <div className="col-5"></div>
@@ -105,7 +94,7 @@ export class CatVsDog extends Component {
                             width="224" height="224" alt="alt"/>
                         <h3>{this.state.prediction}</h3>
                     </div>
-                 )}
+                )}
                 </div>
                 <div className="col-3"></div>
             </div>
